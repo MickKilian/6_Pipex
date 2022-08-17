2cd5dd42450d8054dd279be36d8ea920c47d87d1
Michael BOURGEOIS <mbourgeo@e1r8p4.clusters.42paris.fr>
2022-08-17 23:42:09 +0200

    Changed wording of Path error

--- prim/inc/pipex.h
+++ prim/inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mbourgeo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 05:28:25 by mbourgeo          #+#    #+#             */
-/*   Updated: 2022/08/17 23:09:15 by mbourgeo         ###   ########.fr       */
+/*   Updated: 2022/08/17 23:38:33 by mbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@
 # define ERR_ARGS "Error : exactly 4 arguments must be specified\n"
 # define ERR_FILEHEREDOC "Error with heredoc\n"
 # define ERR_ENV "Comment : Environment empty\n"
-# define ERR_PATHENV "Comment : Env PATH not found\n"
+# define ERR_PATHENV "Comment : PATH not found in Env\n"
 # define ERR_PATHSPLIT "Error while splitting environment path\n"
 # define ERR_REDIR "Redirection Error\n"
 # define ERR_FILE "File Error\n"
